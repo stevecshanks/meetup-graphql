e38d63235f86e7c4b824a0eff61b24e3b778509e
Steve Shanks <steve.c.shanks@gmail.com>
2018-09-29 21:13:32 +0100

    Move schema to .graphql file

--- graphql.php
+++ graphql.php
@@ -2,69 +2,25 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use GraphQL\Utils\BuildSchema;
 use Meetup\DataGenerator;
 use GraphQL\GraphQL;
-use GraphQL\Type\Definition\ObjectType;
-use GraphQL\Type\Definition\Type;
-use GraphQL\Type\Schema;
 
 $dataGenerator = new DataGenerator();
 
-$personType = new ObjectType([
-    'name' => 'Person',
-    'fields' => [
-        'id' => Type::id(),
-        'name' => Type::string(),
-        'companyName' => Type::string(),
-    ]
-]);
-
-$addressType = new ObjectType([
-    'name' => 'Address',
-    'fields' => [
-        'companyName' => Type::string(),
-        'address' => Type::string(),
-        'city' => Type::string(),
-        'postcode' => Type::string(),
-    ]
-]);
-
-$meetupType = new ObjectType([
-    'name' => 'Meetup',
-    'fields' => [
-        'id' => Type::id(),
-        'name' => Type::string(),
-        'location' => $addressType,
-        'start' => Type::string(),
-        'organiser' => $personType,
-        'presenter' => $personType,
-        'attendees' => Type::listOf($personType),
-    ],
-]);
-
-$queryType = new ObjectType([
-    'name' => 'Query',
-    'fields' => [
-        'meetups' => [
-            'type' => Type::listOf($meetupType),
-            'resolve' => function () use ($dataGenerator) {
-                return $dataGenerator->collectionOf(10, 'randomMeetup');
-            }
-        ]
-    ],
-]);
-
-$schema = new Schema([
-    'query' => $queryType,
-]);
+$contents = file_get_contents(__DIR__ . '/schema.graphql');
+$schema = BuildSchema::build($contents);
 
 $rawInput = file_get_contents('php://input');
 $input = json_decode($rawInput, true);
 $query = $input['query'];
-$variableValues = $input['variables'] ?? null;
+
+$rootValue = [
+    'meetups' => $dataGenerator->collectionOf(10, 'randomMeetup'),
+];
 
 try {
-    $result = GraphQL::executeQuery($schema, $query, null, null, $variableValues);
+    $result = GraphQL::executeQuery($schema, $query, $rootValue);
     $output = $result->toArray();
 } catch (Exception $e) {
     $output = [
