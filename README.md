3d1a339ca82d065446a604dad8a14b165dd5e828
Steve Shanks <steve.c.shanks@gmail.com>
2018-09-29 18:20:41 +0100

    Add bargain-basement frontend

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ Learning GraphQL by creating an API for a Meetup.com clone.
 ## Usage
 
 ```shell
+# Run API
 php -S localhost:8080 graphql.php
+
+# Run frontend
+php -S localhost:8081 frontend.php
 ```
 
 ## Example Query
---
+++ frontend.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig = new Twig_Environment($loader);
+
+$client = new Client();
+
+$query = <<<GRAPHQL
+query {
+  meetups {
+    name
+    start
+    location {
+      companyName
+      address
+      city
+      postcode
+    }
+    organiser {
+      name
+    }
+    attendees {
+      id
+    }
+  }
+}
+GRAPHQL;
+
+$response = $client->post('http://localhost:8080', [
+    'json' => [
+        'query' => $query
+    ]
+]);
+
+$json = json_decode($response->getBody()->getContents());
+
+echo $twig->render('index.html.twig', ['meetups' => $json->data->meetups]);
